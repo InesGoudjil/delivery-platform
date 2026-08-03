b2950c5de7fd0148259564ec1de2b2e5083673ca
InesGoudjil <goudjilines25@gmail.com>
2026-08-03 23:50:26 +0100

    added font and improving styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import LandingPage from "@/components/LandingPage";
+import { LoginForm } from "@/components/login/login-form";
+import { SignupForm } from "@/components/signup/signup-form";
 
 export default function Home() {
   return <LandingPage />;
